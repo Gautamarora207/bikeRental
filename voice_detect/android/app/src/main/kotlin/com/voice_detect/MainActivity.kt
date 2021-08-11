58424801424736952af6f3a4d0486665fb626c22
Gautamarora207 <51964347+Gautamarora207@users.noreply.github.com>
2021-08-11 10:47:48 +0530

    Delete voice_detect directory

--- voice_detect/android/app/src/main/kotlin/com/voice_detect/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.voice_detect
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- voice_detect/lib/constants/urlConstants.dart
+++
@@ -1,3 +0,0 @@
-const String baseUrl = 'https://order-fe87e-default-rtdb.firebaseio.com/';
-const String masterUsers = 'masterUsers/';
-const String clientUsers = 'clientUsers/';
